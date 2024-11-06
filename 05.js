d27fcef3c65c18ce67bef123f85f89c9b7a5e401
makhset <beibarysm10@gmail.com>
2024-11-06 15:18:53 +0500

    Add files via upload

---
+++ 05.js
@@ -0,0 +1,27 @@
+// let num1 = prompt("Бырыншы санды енгызз")
+// let num2 = prompt("Екыншы санды енгыз")
+
+// let sum = 0;
+
+// sun = num1 + num2
+// alert("Сыздын енгызген сандарыныздын косындысы = " + sum)
+
+
+// let num1 = -6
+// let num2 = -10
+
+// console.log(num1<=0 && num2>=0) ||(num1>=0 && num2<=0);
+
+
+
+
+
+let num5 = +prompt("Write number:")
+num5 %=2;
+if(num5==0){
+    alert("Сыздын саныныз жуп")
+}
+else{
+    alert("Сыздын саныныз так ")
+}
+ 
